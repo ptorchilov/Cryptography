a026995b42edb7e0fd0a45e7c6054b938a6d36b9
Pavel Torchilov <p.torchilov@itransition.com>
2014-11-28 14:18:24 +0300

    Refactoring.

--- Lab04/Lab04/FiatShamir.cs
+++ Lab04/Lab04/FiatShamir.cs
@@ -2,35 +2,95 @@
 {
     using System;
 
+    /// <summary>
+    /// Calss for realize Fiat-Shamir algorithm.
+    /// </summary>
     public class FiatShamir
     {
+        #region Algorithm params
+
+        /// <summary>
+        /// Gets the Ksi1.
+        /// </summary>
+        /// <value>
+        /// The Ksi1.
+        /// </value>
         public int Ksi1 { get; private set; }
 
+        /// <summary>
+        /// Gets the Ksi2.
+        /// </summary>
+        /// <value>
+        /// The Ksi2.
+        /// </value>
         public int Ksi2 { get; private set; }
 
+        /// <summary>
+        /// Gets the m.
+        /// </summary>
+        /// <value>
+        /// The m.
+        /// </value>
         public int M { get; private set; }
 
+        /// <summary>
+        /// Gets the alpha.
+        /// </summary>
+        /// <value>
+        /// The alpha.
+        /// </value>
         public int Alpha { get; private set; }
 
-        public int Beta { get; private set; }
+        /// <summary>
+        /// Gets the beta.
+        /// </summary>
+        /// <value>
+        /// The beta.
+        /// </value>
+        public int Beta { get; private set; } 
 
-        private readonly Random random;
+        #endregion
 
+        #region Fields
+
+        /// <summary>
+        /// The random
+        /// </summary>
+        private readonly Random random; 
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FiatShamir"/> class.
+        /// </summary>
+        /// <param name="ksi1">The ksi1.</param>
+        /// <param name="ksi2">The ksi2.</param>
         public FiatShamir(int ksi1, int ksi2)
         {
             Ksi1 = ksi1;
             Ksi2 = ksi2;
 
             random = new Random();
-        }
+        } 
 
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Generates the signature.
+        /// </summary>
         public void GenerateSignature()
         {
             M = Ksi1 * Ksi2;
 
             Alpha = random.Next(1, M - 1); //TODO: check this condition
 
-            Beta = (int) Math.Pow(Alpha, 2) % M;
-        }
+            Beta = (int)Math.Pow(Alpha, 2) % M;
+        } 
+
+        #endregion
     }
 }
--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Lab04
+﻿namespace Lab04
 {
-    static class Program
+    using System;
+    using System.Windows.Forms;
+
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
