f071ee0c11006fdd2208f17b5f1a70ef472b0ddd
Pavel Torchilov <p.torchilov@itransition.com>
2014-11-28 14:08:55 +0300

    Add prime tool, start algorithm realization.

--- Lab04/Lab04/FiatShamir.cs
+++ Lab04/Lab04/FiatShamir.cs
@@ -1,10 +1,36 @@
 ﻿namespace Lab04
 {
+    using System;
+
     public class FiatShamir
     {
-        public static void GenerateSignature()
+        public int Ksi1 { get; private set; }
+
+        public int Ksi2 { get; private set; }
+
+        public int M { get; private set; }
+
+        public int Alpha { get; private set; }
+
+        public int Beta { get; private set; }
+
+        private readonly Random random;
+
+        public FiatShamir(int ksi1, int ksi2)
         {
-            
+            Ksi1 = ksi1;
+            Ksi2 = ksi2;
+
+            random = new Random();
+        }
+
+        public void GenerateSignature()
+        {
+            M = Ksi1 * Ksi2;
+
+            Alpha = random.Next(1, M - 1); //TODO: check this condition
+
+            Beta = (int) Math.Pow(Alpha, 2) % M;
         }
     }
 }
